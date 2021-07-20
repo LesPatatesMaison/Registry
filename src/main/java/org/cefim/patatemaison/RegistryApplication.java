d671d3e1ef4ec40091a9ffeeec9276b96174778d
J-Phiz <jp.save@gmail.com>
2021-07-20 17:42:33 +0200

    Rename Java Class

--- src/main/java/org/cefim/patatemaison/RegistryApplication.java
+++ src/main/java/org/cefim/patatemaison/RegistryApplication.java
@@ -2,12 +2,14 @@ package org.cefim.patatemaison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class PatatemaisonApplication {
+public class RegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatatemaisonApplication.class, args);
+		SpringApplication.run(RegistryApplication.class, args);
 	}
 
 }
