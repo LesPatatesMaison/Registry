652d68f3c1bce9b7d02602726aaa58ffca3e6482
J-Phiz <jp.save@gmail.com>
2021-07-20 17:20:37 +0200

    First commit

---
+++ src/main/java/org/cefim/patatemaison/PatatemaisonApplication.java
@@ -0,0 +1,13 @@
+package org.cefim.patatemaison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatatemaisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatatemaisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cefim/patatemaison/PatatemaisonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cefim.patatemaison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatatemaisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
